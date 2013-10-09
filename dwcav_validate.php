4faa4d7717699d605e5d0e9a3cecdb333b175144
= <=>
2013-10-09 19:58:06 +0100

    Check attributes on extension files in meta.xml.

--- dwcav_validate.php
+++ dwcav_validate.php
@@ -28,12 +28,21 @@ function dwcav_meta_xml($dir, $ok_files) {
   	dwcav_error('error', 'meta.xml', "No core defined in meta.xml");
   	return FALSE;
   }
-  //Validate attributes
-  dwcav_xml_attributes($meta_xml->core->attributes(), 'meta.xml');
+  //Validate core attributes
+  dwcav_xml_attributes($meta_xml->core->attributes(), $meta_xml->core->files->location);
   if (!isset($meta_xml->core->files->location)) {
   	dwcav_error('error', 'meta.xml', "No core file is defined in the meta.xml");
   	return FALSE;
   }
+  //Loop over extensions and check
+  foreach ($meta_xml->extension as $extension) {
+    dwcav_xml_attributes($extension->attributes(), $extension->files->location);
+    if (!isset($extension->files->location)) {
+      dwcav_error('error', 'meta.xml', "No file is defined for $extension->attributes()->rowType");
+      continue;
+    }
+  }
+  
 }
 
 function dwcav_xml_attributes($attributes, $file) {
@@ -54,6 +63,7 @@ function dwcav_xml_attributes($attributes, $file) {
   	} else {
   	$function = 'dwcav_xml_attributes_'.$check;
   	  if (function_exists($function)) {
+  	  	//$array[$check] = 't'; //uncomment to give error for each attribute
   	  	$function($array[$check], $file);
   	  }
   	}
@@ -61,18 +71,18 @@ function dwcav_xml_attributes($attributes, $file) {
 }
 
 function dwcav_xml_attributes_ignoreHEaderLines($check, $file) {
-  if(!is_int($check)) {
-  	dwcav_error('error', $file, "ignoreHeaderLines is not an integer for $file in meta.xml");
+  if(!is_int((int)$check)) {
+  	dwcav_error('error', 'meta.xml', "ignoreHeaderLines is not an integer for $file in meta.xml");
   	return;
   }
   if ($check > 1) {
-  	dwcav_error('info', $file, "ignoreHEaderLines seems rather large, are you sure it's correct?");
+  	dwcav_error('info', 'meta.xml', "ignoreHEaderLines seems rather large, are you sure it's correct?");
   }
 }
 
 function dwcav_xml_attributes_rowType($check, $file) {
   if (!filter_var($check, FILTER_VALIDATE_URL)) {
-  	dwcav_error('error', $file, "rowType for $file in meta.xml is not a valid URL");
+  	dwcav_error('error', 'meta.xml', "rowType for $file in meta.xml is not a valid URL");
   }	
 }
 
@@ -83,7 +93,7 @@ function dwcav_xml_attributes_linesTerminatedBy($check, $file) {
     '\n',
   );
   if (!in_array($check, $normal_values)) {
-  	dwcav_error('info', $file, "Non-standard value for linesTerminatedBy in $file");
+  	dwcav_error('info', 'meta.xml', "Non-standard value for linesTerminatedBy in $file");
   }
 }
 
@@ -94,7 +104,7 @@ function dwcav_xml_attributes_fieldsEnclosedBy($check, $file) {
     '"',
   );
   if (!in_array($check, $normal_values)) {
-  	dwcav_error('info', $file, "Non-standard value for fieldsEnclosedBy in $file");
+  	dwcav_error('info', 'meta.xml', "Non-standard value for fieldsEnclosedBy in $file");
   }
 }
 
@@ -104,7 +114,7 @@ function dwcav_xml_attributes_fieldsTerminatedBy($check, $file) {
     ",",
   );
   if (!in_array($check, $normal_values)) {
-  	dwcav_error('info', $file, "Non-standard value for fieldsTerminatedBy in $file");
+  	dwcav_error('info', 'meta.xml', "Non-standard value for fieldsTerminatedBy in $file");
   }
 }
 
