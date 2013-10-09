e11a7bc1bb3638199b06eb2bae3a5e9a98f5928c
= <=>
2013-10-09 18:56:09 +0100

    Initial code for validation.

---
+++ dwcav_validate.php
@@ -0,0 +1,182 @@
+#!/usr/bin/php
+<?php
+
+print "DwC-A Validator by Ed Baker\n";
+
+//Get file
+global $argv;
+$file_name = $argv[1];
+
+$zip = dwcav_openfile($file_name);
+$dir = dwcav_extract($zip, $file_name);
+$dir_list = scandir($dir);
+
+//Start validation of archive
+$ok_files  = dwcav_archive($dir, $dir_list);
+$core_file = dwcav_meta_xml($dir, $ok_files);
+
+$errors = array();
+
+//Delve into the meta.xml
+function dwcav_meta_xml($dir, $ok_files) {
+  $meta = file_get_contents("$dir/meta.xml");
+  $meta_xml  = new SimpleXMLElement($meta);
+  //print_r($meta_xml->core);
+  
+  //Check for core file data
+  if (!isset($meta_xml->core)) {
+  	dwcav_error('error', 'meta.xml', "No core defined in meta.xml");
+  	return FALSE;
+  }
+  //Validate attributes
+  dwcav_xml_attributes($meta_xml->core->attributes(), 'meta.xml');
+  if (!isset($meta_xml->core->files->location)) {
+  	dwcav_error('error', 'meta.xml', "No core file is defined in the meta.xml");
+  	return FALSE;
+  }
+}
+
+function dwcav_xml_attributes($attributes, $file) {
+  $array = (array) $attributes;
+  $array = $array['@attributes'];
+  $checks = array(
+    'encoding',
+    'linesTerminatedBy',
+    'fieldsTerminatedBy',
+    'fieldsEnclosedBy',
+    'ignoreHeaderLines',
+    'rowType',
+  );
+  
+  foreach ($checks as $check) {
+  	if (!isset($array[$check])) {
+  	  dwcav_error('error', 'meta.xml', "$check not defined in core attributes");
+  	} else {
+  	$function = 'dwcav_xml_attributes_'.$check;
+  	  if (function_exists($function)) {
+  	  	$function($array[$check], $file);
+  	  }
+  	}
+  }
+}
+
+function dwcav_xml_attributes_ignoreHEaderLines($check, $file) {
+  if(!is_int($check)) {
+  	dwcav_error('error', $file, "ignoreHeaderLines is not an integer for $file in meta.xml");
+  	return;
+  }
+  if ($check > 1) {
+  	dwcav_error('info', $file, "ignoreHEaderLines seems rather large, are you sure it's correct?");
+  }
+}
+
+function dwcav_xml_attributes_rowType($check, $file) {
+  if (!filter_var($check, FILTER_VALIDATE_URL)) {
+  	dwcav_error('error', $file, "rowType for $file in meta.xml is not a valid URL");
+  }	
+}
+
+function dwcav_xml_attributes_linesTerminatedBy($check, $file) {
+  $normal_values = array(
+    '\r\n',
+    '\r',
+    '\n',
+  );
+  if (!in_array($check, $normal_values)) {
+  	dwcav_error('info', $file, "Non-standard value for linesTerminatedBy in $file");
+  }
+}
+
+function dwcav_xml_attributes_fieldsEnclosedBy($check, $file) {
+  $normal_values = array(
+    "",
+    "'",
+    '"',
+  );
+  if (!in_array($check, $normal_values)) {
+  	dwcav_error('info', $file, "Non-standard value for fieldsEnclosedBy in $file");
+  }
+}
+
+function dwcav_xml_attributes_fieldsTerminatedBy($check, $file) {
+  $normal_values = array(
+    "\t",
+    ",",
+  );
+  if (!in_array($check, $normal_values)) {
+  	dwcav_error('info', $file, "Non-standard value for fieldsTerminatedBy in $file");
+  }
+}
+
+//Check overall archive is ok
+function dwcav_archive($dir, $dir_list) {
+  global $errors;
+  $ok_files = array();
+  //meta.xml is present;
+  if (in_array('meta.xml', $dir_list)){
+  	print "OK!\n";
+  } else {
+  	dwcav_error('error', 'archive', 'meta.xml is not present');
+  }
+  foreach ($dir_list as $file) {
+  	$file_ok = TRUE;
+  	if ($file == '.' /*|| $file = '..' || $file == 'meta.xml'*/) { //Removed for DEBUG
+  		$file_ok = FALSE;
+  		continue;
+  	}
+  	//Check is a file
+  	if (!is_file($dir.'/'.$file)) {
+  	  $file_ok = FALSE;
+  	  dwcav_error('error', 'archive', "$file is not a file");
+  	}
+  	if (is_dir($dir.'/'.$file)) {
+  	  $file_ok = FALSE;
+  	  dwcav_error('error', 'archive', "$file is a directory - zip should not contain directories");
+  	}
+  	if (substr($file, -4 ) != '.txt') {
+  		$file_ok = FALSE;
+  	  dwcav_error('error', 'archive', "$file is not a text file or does not have .txt extension");
+  	}
+  }
+  if($file_ok) {
+  	$ok_files[] = $file;
+  }
+  return $ok_files;
+}
+
+function dwcav_val_meta_xml() {
+  global $errors;
+
+  	
+}
+
+function dwcav_error($level, $section, $message) {
+	print "[$section] ($level): $message\n";
+	global $errors;
+	$errors[] = array($level, $section, $message);
+}
+
+function dwcav_openfile($file_name) {
+  print "Trying to open ".$file_name."...";
+  $zip = new ZipArchive;
+  $is_open = $zip->open($file_name);
+  if ($is_open === TRUE) {
+  	print "OK!\n";
+  	return $zip;
+  } else {
+  	print "FAIL!\n";
+  	exit;
+  }
+}
+
+function dwcav_extract(&$zip, $file_name) {
+  print "Extracting to ";
+  $tmp_path = sys_get_temp_dir().'/dwca/';
+  print $tmp_path."\n";
+  $zip->extractTo($tmp_path);
+  $zip->close();
+  $dir_listing = scandir($tmp_path);
+  return $tmp_path.$dir_listing[2]."/";
+}
+
+print "\n";
